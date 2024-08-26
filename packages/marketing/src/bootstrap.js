d5251453e8bd9d283c791032ff18bad5d7a7e0d3
Kornel Lokos <tokapeb@gmail.com>
2024-08-26 15:24:10 +0200

    Restart deployment workflows.

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -18,3 +18,4 @@ if (process.env.NODE_ENV === 'development') {
 // We are running through container
 // and we should export the mount function
 export { mount };
+// This is the bootstrap file for the marketing app.
